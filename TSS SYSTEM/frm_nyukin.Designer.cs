7e5bb2ebc8a913a5292d0f74475bc2cba14beaeb
okuda@tokunaga-seisakusyo.co.jp <okuda@tokunaga-seisakusyo.co.jp>
2015-09-04 21:18:10 +0900

    入金処理編集中

--- TSS SYSTEM/frm_nyukin.Designer.cs
+++ TSS SYSTEM/frm_nyukin.Designer.cs
@@ -30,8 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_nyukin));
             this.btn_syuuryou = new System.Windows.Forms.Button();
-            this.tb_bikou2 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.tb_nyukin_goukei = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.lbl_seikyuu = new System.Windows.Forms.Label();
@@ -40,11 +38,21 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.dgv_m = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.splitContainer4 = new System.Windows.Forms.SplitContainer();
             this.tb_nyukin_no = new System.Windows.Forms.TextBox();
             this.tb_torihikisaki_name = new System.Windows.Forms.TextBox();
             this.btn_touroku = new System.Windows.Forms.Button();
             this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.tb_update_datetime = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tb_update_user_cd = new System.Windows.Forms.TextBox();
+            this.tb_create_user_cd = new System.Windows.Forms.TextBox();
+            this.tb_create_datetime = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.btn_hardcopy = new System.Windows.Forms.Button();
@@ -77,25 +85,7 @@
             this.btn_syuuryou.TabIndex = 1;
             this.btn_syuuryou.Text = "終了";
             this.btn_syuuryou.UseVisualStyleBackColor = true;
-            // 
-            // tb_bikou2
-            // 
-            this.tb_bikou2.Location = new System.Drawing.Point(46, 3);
-            this.tb_bikou2.MaxLength = 128;
-            this.tb_bikou2.Name = "tb_bikou2";
-            this.tb_bikou2.Size = new System.Drawing.Size(513, 19);
-            this.tb_bikou2.TabIndex = 5;
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.NavajoWhite;
-            this.textBox2.Location = new System.Drawing.Point(10, 3);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(36, 19);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.TabStop = false;
-            this.textBox2.Text = "備考";
+            this.btn_syuuryou.Click += new System.EventHandler(this.btn_syuuryou_Click_1);
             // 
             // tb_nyukin_goukei
             // 
@@ -137,6 +127,8 @@
             this.tb_torihikisaki_cd.Name = "tb_torihikisaki_cd";
             this.tb_torihikisaki_cd.Size = new System.Drawing.Size(51, 19);
             this.tb_torihikisaki_cd.TabIndex = 1;
+            this.tb_torihikisaki_cd.DoubleClick += new System.EventHandler(this.tb_torihikisaki_cd_DoubleClick);
+            this.tb_torihikisaki_cd.Validating += new System.ComponentModel.CancelEventHandler(this.tb_torihikisaki_cd_Validating_1);
             // 
             // tb_nyukin_date
             // 
@@ -145,6 +137,7 @@
             this.tb_nyukin_date.Name = "tb_nyukin_date";
             this.tb_nyukin_date.Size = new System.Drawing.Size(75, 19);
             this.tb_nyukin_date.TabIndex = 2;
+            this.tb_nyukin_date.Validating += new System.ComponentModel.CancelEventHandler(this.tb_nyukin_date_Validating_1);
             // 
             // textBox1
             // 
@@ -166,17 +159,44 @@
             this.textBox6.Size = new System.Drawing.Size(100, 19);
             this.textBox6.TabIndex = 5;
             this.textBox6.TabStop = false;
-            this.textBox6.Text = "売上計上日";
+            this.textBox6.Text = "入金日";
             // 
             // dgv_m
             // 
             this.dgv_m.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_m.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
             this.dgv_m.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgv_m.Location = new System.Drawing.Point(0, 0);
             this.dgv_m.Name = "dgv_m";
             this.dgv_m.RowTemplate.Height = 21;
             this.dgv_m.Size = new System.Drawing.Size(880, 320);
             this.dgv_m.TabIndex = 0;
+            this.dgv_m.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_m_CellEndEdit);
+            this.dgv_m.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dgv_m_CellValidating);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "入金区分";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "入金区分名称";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "入金額";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "備考";
+            this.Column4.Name = "Column4";
             // 
             // splitContainer4
             // 
@@ -193,8 +213,6 @@
             // 
             // splitContainer4.Panel2
             // 
-            this.splitContainer4.Panel2.Controls.Add(this.tb_bikou2);
-            this.splitContainer4.Panel2.Controls.Add(this.textBox2);
             this.splitContainer4.Panel2.Controls.Add(this.tb_nyukin_goukei);
             this.splitContainer4.Panel2.Controls.Add(this.textBox10);
             this.splitContainer4.Size = new System.Drawing.Size(880, 353);
@@ -210,6 +228,7 @@
             this.tb_nyukin_no.Name = "tb_nyukin_no";
             this.tb_nyukin_no.Size = new System.Drawing.Size(75, 19);
             this.tb_nyukin_no.TabIndex = 0;
+            this.tb_nyukin_no.Validating += new System.ComponentModel.CancelEventHandler(this.tb_nyukin_no_Validating);
             // 
             // tb_torihikisaki_name
             // 
@@ -229,6 +248,7 @@
             this.btn_touroku.TabIndex = 0;
             this.btn_touroku.Text = "登録";
             this.btn_touroku.UseVisualStyleBackColor = true;
+            this.btn_touroku.Click += new System.EventHandler(this.btn_touroku_Click);
             // 
             // splitContainer3
             // 
@@ -241,6 +261,12 @@
             // 
             // splitContainer3.Panel1
             // 
+            this.splitContainer3.Panel1.Controls.Add(this.tb_update_datetime);
+            this.splitContainer3.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer3.Panel1.Controls.Add(this.tb_update_user_cd);
+            this.splitContainer3.Panel1.Controls.Add(this.tb_create_user_cd);
+            this.splitContainer3.Panel1.Controls.Add(this.tb_create_datetime);
+            this.splitContainer3.Panel1.Controls.Add(this.textBox9);
             this.splitContainer3.Panel1.Controls.Add(this.lbl_seikyuu);
             this.splitContainer3.Panel1.Controls.Add(this.tb_torihikisaki_cd);
             this.splitContainer3.Panel1.Controls.Add(this.tb_nyukin_date);
@@ -256,6 +282,68 @@
             this.splitContainer3.Size = new System.Drawing.Size(880, 435);
             this.splitContainer3.SplitterDistance = 78;
             this.splitContainer3.TabIndex = 7;
+            // 
+            // tb_update_datetime
+            // 
+            this.tb_update_datetime.BackColor = System.Drawing.Color.Gainsboro;
+            this.tb_update_datetime.Location = new System.Drawing.Point(743, 51);
+            this.tb_update_datetime.Name = "tb_update_datetime";
+            this.tb_update_datetime.ReadOnly = true;
+            this.tb_update_datetime.Size = new System.Drawing.Size(127, 19);
+            this.tb_update_datetime.TabIndex = 53;
+            this.tb_update_datetime.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox2.Location = new System.Drawing.Point(660, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(38, 19);
+            this.textBox2.TabIndex = 48;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "作成";
+            // 
+            // tb_update_user_cd
+            // 
+            this.tb_update_user_cd.BackColor = System.Drawing.Color.Gainsboro;
+            this.tb_update_user_cd.Location = new System.Drawing.Point(698, 51);
+            this.tb_update_user_cd.Name = "tb_update_user_cd";
+            this.tb_update_user_cd.ReadOnly = true;
+            this.tb_update_user_cd.Size = new System.Drawing.Size(45, 19);
+            this.tb_update_user_cd.TabIndex = 52;
+            this.tb_update_user_cd.TabStop = false;
+            // 
+            // tb_create_user_cd
+            // 
+            this.tb_create_user_cd.BackColor = System.Drawing.Color.Gainsboro;
+            this.tb_create_user_cd.Location = new System.Drawing.Point(698, 32);
+            this.tb_create_user_cd.Name = "tb_create_user_cd";
+            this.tb_create_user_cd.ReadOnly = true;
+            this.tb_create_user_cd.Size = new System.Drawing.Size(45, 19);
+            this.tb_create_user_cd.TabIndex = 49;
+            this.tb_create_user_cd.TabStop = false;
+            // 
+            // tb_create_datetime
+            // 
+            this.tb_create_datetime.BackColor = System.Drawing.Color.Gainsboro;
+            this.tb_create_datetime.Location = new System.Drawing.Point(743, 32);
+            this.tb_create_datetime.Name = "tb_create_datetime";
+            this.tb_create_datetime.ReadOnly = true;
+            this.tb_create_datetime.Size = new System.Drawing.Size(127, 19);
+            this.tb_create_datetime.TabIndex = 50;
+            this.tb_create_datetime.TabStop = false;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBox9.Location = new System.Drawing.Point(660, 51);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(38, 19);
+            this.textBox9.TabIndex = 51;
+            this.textBox9.TabStop = false;
+            this.textBox9.Text = "更新";
             // 
             // textBox3
             // 
@@ -300,6 +388,7 @@
             this.btn_hardcopy.TabIndex = 0;
             this.btn_hardcopy.TabStop = false;
             this.btn_hardcopy.UseVisualStyleBackColor = true;
+            this.btn_hardcopy.Click += new System.EventHandler(this.btn_hardcopy_Click_1);
             // 
             // splitContainer1
             // 
@@ -341,6 +430,7 @@
             this.MinimumSize = new System.Drawing.Size(900, 600);
             this.Name = "frm_nyukin";
             this.Text = "入金処理";
+            this.Load += new System.EventHandler(this.frm_nyukin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_m)).EndInit();
             this.splitContainer4.Panel1.ResumeLayout(false);
             this.splitContainer4.Panel2.ResumeLayout(false);
@@ -368,8 +458,6 @@
         #endregion
 
         private System.Windows.Forms.Button btn_syuuryou;
-        private System.Windows.Forms.TextBox tb_bikou2;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox tb_nyukin_goukei;
         private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.Label lbl_seikyuu;
@@ -388,6 +476,16 @@
         private System.Windows.Forms.Button btn_hardcopy;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox tb_update_datetime;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tb_update_user_cd;
+        private System.Windows.Forms.TextBox tb_create_user_cd;
+        private System.Windows.Forms.TextBox tb_create_datetime;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
 
     }
 }
