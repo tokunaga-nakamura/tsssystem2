1227170716bf41986ef0067ad167232b6afb89a9
okuda@tokunaga-seisakusyo.co.jp <okuda@tokunaga-seisakusyo.co.jp>
2015-09-25 18:00:45 +0900

    部品入出庫履歴印刷

---
+++ TSS SYSTEM/frm_buhin_nyusyukko_preview.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSS_SYSTEM
+{
+    public partial class frm_buhin_nyusyukko_preview : Form
+    {
+        TssSystemLibrary tss = new TssSystemLibrary();
+        DataTable w_dt_buhin_nyusyukko = new DataTable();
+
+        //ヘッダーの受け渡し変数の定義
+        public string w_yyyymmdd;
+        public string w_hd10;//処理日1
+        public string w_hd11;//処理日2
+        public string w_hd20;//取引先CD1
+        public string w_hd21;//取引先CD2
+        public string w_hd30;//部品CD1
+        public string w_hd31;//部品CD2
+
+        //親画面から参照できるプロパティを作成
+        public DataTable fld_dt = new DataTable();   //印刷する明細データ
+
+        public DataTable ppt_dt
+        {
+            get
+            {
+                return fld_dt;
+            }
+            set
+            {
+                fld_dt = value;
+            }
+        }
+        
+        public frm_buhin_nyusyukko_preview()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_syuuryou_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frm_buhin_nyusyukko_preview_Load(object sender, EventArgs e)
+        {
+            rpt_buhin_nyusyukko rpt = new rpt_buhin_nyusyukko();
+            //レポートへデータを受け渡す
+            rpt.DataSource = ppt_dt;
+            rpt.w_yyyymmdd = w_yyyymmdd;
+            rpt.w_hd10 = w_hd10;
+            rpt.w_hd11 = w_hd11;
+            rpt.w_hd20 = w_hd20;
+            rpt.w_hd21 = w_hd21;
+            rpt.w_hd30 = w_hd30;
+            rpt.w_hd31 = w_hd31;
+
+
+            rpt.Run();
+            this.vwr.Document = rpt.Document;
+        }
+
+        private void btn_hardcopy_Click(object sender, EventArgs e)
+        {
+            tss.HardCopy();
+        }
+    }
+}
--- TSS SYSTEM/rpt_buhin_nyusyukko.cs
+++ TSS SYSTEM/rpt_buhin_nyusyukko.cs
@@ -11,6 +11,17 @@ namespace TSS_SYSTEM
     /// </summary>
     public partial class rpt_buhin_nyusyukko : GrapeCity.ActiveReports.SectionReport
     {
+        TssSystemLibrary tss = new TssSystemLibrary();
+
+        //ヘッダーの受け渡し変数の定義
+        public string w_yyyymmdd;
+        public string w_hd10;
+        public string w_hd11;
+        public string w_hd20;
+        public string w_hd21;
+        public string w_hd30;
+        public string w_hd31;
+
 
         public rpt_buhin_nyusyukko()
         {
@@ -27,6 +38,15 @@ namespace TSS_SYSTEM
             this.PageSettings.Margins.Left = GrapeCity.ActiveReports.SectionReport.CmToInch(1.0f);   //左マージン
             this.PageSettings.Margins.Right = GrapeCity.ActiveReports.SectionReport.CmToInch(1.0f);  //右マージン
             this.PageSettings.Margins.Bottom = GrapeCity.ActiveReports.SectionReport.CmToInch(1.0f);   //上マージン
+
+            tb_today.Text = DateTime.Today.ToShortDateString();
+            tb_syoribi1.Text = w_hd10;
+            tb_syoribi2.Text = w_hd11;
+            tb_torihikisaki1.Text = w_hd20;
+            tb_torihikisaki2.Text = w_hd21;
+            tb_buhin_cd1.Text = w_hd30;
+            tb_buhin_cd2.Text = w_hd31;
+
 
          
         }
