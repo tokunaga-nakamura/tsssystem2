eae7194d188a293f97b44dd0b8d973945d5d6ab9
okuda@tokunaga-seisakusyo.co.jp <okuda@tokunaga-seisakusyo.co.jp>
2016-05-11 16:41:24 +0900

    ぷｓっゆ

--- TSS SYSTEM/frm_seisan_koutei_m.Designer.cs
+++ TSS SYSTEM/frm_seisan_koutei_m.Designer.cs
@@ -381,7 +381,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 4);
+            this.button1.Location = new System.Drawing.Point(184, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 97;
